8b00b961aa234074cf133d7f9a303e3809e44375
CaesarRulezzz <caesarrulezzz@gmail.com>
2022-07-07 21:59:40 +0300

    Managed to get unzip flags right

--- download_base.sh
+++ download_base.sh
@@ -4,7 +4,7 @@ rm *.zip -f
 echo "Checking for updates"
 python3 main.py
 echo "Unzipping"
-unzip -o *.zip -d /mnt/REGIST
+unzip -j -o \*.zip -d /mnt/REGIST
 echo "Removing unnesessary files from SD"
 rm /mnt/REGIST/*.doc -f
 echo "Cleaning downloads"
