5a790c83bad258b1e8b4910a35c1c7198b246278
CaesarRulezzz <caesarrulezzz@gmail.com>
2022-08-02 22:24:26 +0300

    shellcheck fixes, fix unzip options

--- download_base.sh
+++ download_base.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Cleaning downloads"
-rm *.zip -f
+rm ./*.zip -f
 echo "Checking for updates"
 python3 main.py
 echo "Unzipping"
@@ -8,4 +8,4 @@ unzip -j -o \*.zip -d /mnt/REGIST
 echo "Removing unnesessary files from SD"
 rm /mnt/REGIST/*.doc -f
 echo "Cleaning downloads"
-rm *.zip -f
+rm ./*.zip -f
